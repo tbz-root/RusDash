7155a866d2c377e0b255d1ed9ef6c3398293f14e
tbz-root <vladislavmaksimkin910@gmail.com>
2026-08-30 16:46:12 +0200

    сука я еблан

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,71 @@
+#include <matjson.hpp>
+#include <Geode/utils/web.hpp>
+#include <Geode/loader/Event.hpp>
+#include <chrono>
+
 using namespace geode::prelude;
+
+static TaskHolder<web::WebResponse> s_themeTask;
 
 #include <Geode/modify/MenuLayer.hpp>
 class $modify(MyMenuLayer, MenuLayer)
 {
     bool init()
     {
+
+        std::string myVersion = "v1.0.2";
+
+        matjson::Value json = matjson::makeObject({
+            {"modVersion", myVersion},
+        });
+
+        auto req = web::WebRequest();
+        req.header("Content-Type", "application/json");
+        req.bodyJSON(json);
+        req.timeout(std::chrono::seconds(15));
+
+        std::string baseUrl = Mod::get()->getSettingValue<bool>("use-mirror") ? "https://www.rustps.online/database" : "https://rustps.online/database";
+        std::string url = baseUrl + "/getUpdates.php";
+
+        s_themeTask.spawn(
+            req.post(url),
+            [](web::WebResponse res) {
+                bool isSuccess = false;
+
+                if (res.ok()) {
+                    std::string responseStr = res.string().unwrapOr("false");
+                    
+                    while (!responseStr.empty() && (responseStr.back() == '\n' || responseStr.back() == '\r' || responseStr.back() == ' ')) {
+                        responseStr.pop_back();
+                    }
+                    
+                    if (responseStr == "true") {
+                        isSuccess = true;
+                    }
+                }
+
+                if (!isSuccess) {
+
+                    Loader::get()->queueInMainThread([]() {
+                        FLAlertLayer::create(
+                            "Update Required",                       
+                            "Please, update RusDash Geode Mod or delete it. The game will close now!", 
+                            "OK"                                    
+                        )->show();
+                    });
+
+                    CCDirector::sharedDirector()->getScheduler()->scheduleSelector(
+                        schedule_selector(MyMenuLayer::onForceCloseGame),
+                        CCDirector::sharedDirector()->getRunningScene(),
+                        1.0f,
+                        0,
+                        0.0f,
+                        false
+                    );
+                }
+            }
+        );
+
         if (!MenuLayer::init())
         {
             return false;
@@ -30,5 +91,10 @@ class $modify(MyMenuLayer, MenuLayer)
     void onOpenSettings(CCObject *)
     {
         openSettingsPopup(Mod::get());
+    }
+
+    void onForceCloseGame(float dt)
+    {
+        exit(0); 
     }
 };
